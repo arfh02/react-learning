65d1f7d88a9e80a606b9f15b0ff12e042434cf42
arfh02 <sabilajasmin3@gmail.com>
2025-11-23 16:28:43 +0700

    done after fixing : 03-writing-test

--- Belajar Dasar Pemrograman JavaScript/final-assessment/03-writing-test/index.test.js
+++ Belajar Dasar Pemrograman JavaScript/final-assessment/03-writing-test/index.test.js
@@ -15,34 +15,6 @@ describe('sum function', () => {
         const expectedValue = 2;
         assert.equal(actualValue, expectedValue);
     });
-
-    it('should throw an error if string passed on a parameter', () => {
-        const potentialErrorToBeThrew = () => {
-            // Arrange
-            const operandA = '5';
-            const operandB = 4;
-
-            // Action
-            sum(operandA, operandB);
-        };
-
-        // Assert
-        assert.throws(potentialErrorToBeThrew, Error);
-    });
-
-    it('should throw an error if string passed on b parameter', () => {
-        const potentialErrorToBeThrew = () => {
-            // Arrange
-            const operandA = 10;
-            const operandB = '8';
-
-            // Action
-            sum(operandA, operandB);
-        };
-
-        // Assert
-        assert.throws(potentialErrorToBeThrew, Error);
-    });
 })
 
 
