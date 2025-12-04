6367b2a0a08d5f6349e7531fc998fa596d606520
arfh02 <sabilajasmin3@gmail.com>
2025-12-04 15:02:12 +0700

    Submission :  Menggunakan localStorage sebagai Penyimpanan

--- Belajar Membuat Front-End Web untuk Pemula/submission-bookshelf-app/main.js
+++ Belajar Membuat Front-End Web untuk Pemula/submission-bookshelf-app/main.js
@@ -1,2 +1,29 @@
-// Do your work here...
-console.log('Hello, world!');
+const books = [];
+const SAVED_EVENT = 'saved-book';
+const STORAGE_KEY = 'BOOKSHELF_APPS';
+
+function generateBookObject(id, title, author, year, isCompleted) {
+    return {
+        id,
+        title,
+        author,
+        year,
+        isCompleted
+    }
+}
+
+function saveData() {
+    if (isStorageExist()) {
+        const parsed = JSON.stringify(books);
+        localStorage.setItem(STORAGE_KEY, parsed);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+}
+
+function isStorageExist() {
+    if (typeof (Storage) === undefined) {
+        alert('Browser kamu tidak mendukung local storage');
+        return false;
+    }
+    return true;
+}
