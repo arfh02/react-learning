a8373bd96eab5ca0fcfe1ae78e2ffefcb21fed57
arfh02 <sabilajasmin3@gmail.com>
2025-12-06 02:43:34 +0700

    Submission : Mampu Menambahkan Buku

--- Belajar Membuat Front-End Web untuk Pemula/submission-bookshelf-app/main.js
+++ Belajar Membuat Front-End Web untuk Pemula/submission-bookshelf-app/main.js
@@ -1,6 +1,33 @@
 const books = [];
+const RENDER_EVENT = 'render-bookshelf';
 const SAVED_EVENT = 'saved-book';
 const STORAGE_KEY = 'BOOKSHELF_APPS';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm = document.getElementById('bookForm');
+    submitForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        addBook();
+    });
+});
+
+function addBook() {
+    const title = document.getElementById('bookFormTitle').value;
+    const author = document.getElementById('bookFormAuthor').value;
+    const year = document.getElementById('bookFormYear').value;
+    const isCompleted = document.getElementById('bookFormIsComplete').checked;
+
+    const id = generateId();
+    const bookObject = generateBookObject(id, title, author, year, isCompleted);
+    books.push(bookObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
+}
+
+function generateId() {
+    return +new Date();
+}
 
 function generateBookObject(id, title, author, year, isCompleted) {
     return {
@@ -10,6 +37,40 @@ function generateBookObject(id, title, author, year, isCompleted) {
         year,
         isCompleted
     }
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+    const unreadBook = document.getElementById('incompleteBookList');
+    unreadBook.innerHTML = '';
+
+    for (const bookItem of books) {
+        const bookElement = makeBook(bookItem);
+        unreadBook.append(bookElement);
+    }
+});
+
+function makeBook(bookObject) {
+    const textTitle = document.createElement('h3');
+    textTitle.innerText = bookObject.title;
+
+    const textAuthor = document.createElement('p');
+    textAuthor.innerText = bookObject.author;
+
+    const textYear = document.createElement('p');
+    textYear.innerText = bookObject.year;
+
+    const textContainer = document.createElement('div');
+    textContainer.append(textTitle, textAuthor, textYear);
+
+    const container = document.createElement('div');
+    container.setAttribute('data-testid', `bookItem`);
+    container.setAttribute('data-bookid', bookObject.id);
+    container.append(textContainer);
+
+    const incompleteList = document.getElementById('incompleteBookList');
+    incompleteList.append(container);
+
+    return container;
 }
 
 function saveData() {
