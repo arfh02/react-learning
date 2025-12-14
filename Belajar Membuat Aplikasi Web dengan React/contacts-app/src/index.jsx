141584188e9b11c8a212cb3e16a5dd5a7896d9e2
arfh02 <sabilajasmin3@gmail.com>
2025-12-14 15:30:05 +0700

    Latihan Studi Kasus: Menambahkan Fitur Hapus Kontak

--- Belajar Membuat Aplikasi Web dengan React/contacts-app/src/index.jsx
+++ Belajar Membuat Aplikasi Web dengan React/contacts-app/src/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import ContactApp from './components/ContactApp';
 
 // styling
-import './style.css';
+import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<ContactApp />);
